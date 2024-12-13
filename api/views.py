eadfbb9ee32618d698099c955a2e25f8fd8f9250
Abdulbosit Gofforaliyev <abdulbosit.go@gmail.com>
2024-12-13 08:44:34 +0500

    day 9 of DRF

--- api/views.py
+++ api/views.py
@@ -2,6 +2,7 @@ from django.db.models import Max
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import generics
+from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 
 from .models import Order, Product
@@ -34,19 +35,22 @@ class UserOrderList(generics.ListAPIView):
         return super().get_queryset().filter(user=self.request.user)
 
 
-@api_view(["GET"])
-def product_info(request):
-    products = Product.objects.all()
-    data = {
-        "products": products,
-        "count": products.count,
-        "max_price": products.aggregate(max_price=Max("price"))["max_price"],
-    }
-    serializer = ProductInfoSerializer(data)
-    return Response(serializer.data)
+
+
+class ProductInfo(APIView):
+    def get(self, request):
+        products = Product.objects.all()
+        data = {
+            "products": products,
+            "count": products.count,
+            "max_price": products.aggregate(max_price=Max("price"))["max_price"],
+        }
+        serializer = ProductInfoSerializer(data)
+        return Response(serializer.data)
 
 
 product_list = ProductList.as_view()
 product_detail = ProductDetail.as_view()
+product_info = ProductInfo.as_view()
 order_list = OrderList.as_view()
 user_order_list = UserOrderList.as_view()
